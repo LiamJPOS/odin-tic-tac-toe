f2289ac57280c1a16caec3d30f834c9ef94d942f
liam <paledeepened@gmail.com>
2024-10-15 12:15:46 +0100

    Display correct image of player token when they are changed

--- script.js
+++ script.js
@@ -1,104 +1,18 @@
-function Cell() {
-    let cellValue = 0
-    const getValue = () => cellValue;
-    //Value is either X or O representing either player's symbol, or 0 for empty cell
-    const setValue = (value) => {
-        if(value === "X" || value === "O"){
-            cellValue = value; 
-        }
-    }
-    
-    return { 
-        getValue,
-        setValue
-    };
-}
+function Player(defaultToken) {
+    let token = defaultToken
 
-function Gameboard() {
-    const rows = 3;
-    const cols = 3;
-    const board = [];
-    
-    for(let i = 0; i < rows; i++){
-        board.push([])
-        for(let j = 0; j < cols; j++){
-            board[i].push(Cell())
-        }
-    }
+     // Counters for player's progress on rows, columns, and diagonals
+     const rowCounters = [0, 0, 0];
+     const colCounters = [0, 0, 0];
+     const diagCounters = [0, 0];  // diagCounters[0] = top-left to bottom-right, diagCounters[1] = top-right to bottom-left
 
-    const getRowCount = () => rows
+     let winCount = 0
 
-    const getColCount = () => cols
-    
-    const printBoard = function() {
-        let rowValues = []
-        console.group("Tic Tac Toe Board");
-        for(let row of board) {
-            for(cell of row) {
-                rowValues.push(cell.getValue())
-            }
-            console.log(rowValues.join("|"));
-            rowValues = []    
-        }
-        console.groupEnd();
-        console.log("\n"); //Seperate boards on multiple calls
-    }
+     const assignToken = (choice) => token = choice;
 
-    const updateBoard = (move, playerToken) => {
-        const row = move[0];
-        const col = move[1];
-        board[row][col].setValue(playerToken);
-    } 
+     const getToken = () => token;
 
-    const validateMove = (move) => {
-        const row = move[0];
-        const col = move[1];
-        return board[row][col].getValue() === 0;
-    }
-    
-
-    return {
-        printBoard,
-        updateBoard,
-        validateMove,
-        getRowCount,
-        getColCount,
-    }
-}
-
-function Player() {
-    
-    let name = '';
-    let token = '';
-
-    // Counters for player's progress on rows, columns, and diagonals
-    const rowCounters = [0, 0, 0];
-    const colCounters = [0, 0, 0];
-    const diagCounters = [0, 0];  // diagCounters[0] = top-left to bottom-right, diagCounters[1] = top-right to bottom-left
-
-    const setName = (input) => name = input; 
-
-    const getName = () => name;
-
-    const assignToken = (choice) => token = choice;
-
-    const chooseToken = function() {
-        let choice = ''
-        while(choice !== "X" && choice !== "O"){
-            choice = prompt(`${name}, choose X or O`);
-        }
-        return choice;
-    }
-    
-    const getToken = () => token;
-
-    const makeMove = function() {
-        let move = prompt(`${name} Make your move. Choose row and then column. Like 0,2 for middle right.`).split(",");
-        move = move.map(Number); //Convert string input from prompt into numbers
-        return move;
-    }
-
-    const updateCounters = (move) => {
+     const updateCounters = (move) => {
         const row = move[0];
         const col = move[1];
 
@@ -121,152 +35,143 @@ function Player() {
         return rowCounters[row] === 3 || colCounters[col] === 3 || diagCounters[0] === 3 || diagCounters[1] === 3;
     }
 
+    const increaseWinCount = () => {winCount++};
+
     return {
-        setName,
-        getName,
-        getToken,
         assignToken,
-        chooseToken,
-        makeMove,
+        getToken,
         updateCounters,
-        winCheck
+        winCheck,
+        increaseWinCount
+    }
+}
+
+function Cell() {
+    let cellValue = null
+
+    const getValue = () => cellValue;
+
+    //Value is either X or O representing either player's symbol, or null for empty cell
+    const setValue = (value) => {
+        if(value === "X" || value === "O"){
+            cellValue = value; 
+        }
+    }
+
+    return { 
+        getValue,
+        setValue
     };
 }
 
-function UIConroller() {
-    const setNameBlock = document.getElementById("set-name-block");
-    const p1Info = document.getElementById("p1-info");
-    const p2Info = document.getElementById("p2-info");
-    const p1DisplayName = document.getElementById("p1-display-name");
-    const p2DisplayName = document.getElementById("p2-display-name");
-    const gameContainer = document.getElementById("game-container");
-    
-    const createBoardGrid = function(rows, cols) {
-        for(let i = 0; i < rows; i++){
-            const rowDiv = document.createElement("div");
-            rowDiv.classList.add('row-div');
+function Gameboard() {
+    const rows = 3;
+    const cols = 3;
+    const board = [];
 
-            for(let j = 0; j < cols; j++){
-                const cellBtn = document.createElement("button");
-                cellBtn.classList.add("cell");
-                cellBtn.id = `cell-${i}-${j}`;
-                
-                rowDiv.appendChild(cellBtn);
+    for(let i = 0; i < rows; i++){
+        board.push([])
+        for(let j = 0; j < cols; j++){
+            board[i].push(Cell())
+        }
+    }
+
+    const printBoard = function() {
+        let rowValues = []
+        console.group("Tic Tac Toe Board");
+        for(let row of board) {
+            for(let cell of row) {
+                rowValues.push(cell.getValue())
             }
-        gameContainer.appendChild(rowDiv);
-        }            
+            console.log(rowValues.join("|"));
+            rowValues = []    
+        }
+        console.groupEnd();
+        console.log("\n"); //Seperate boards on multiple calls
     }
 
-    //Open a dialog
-    const openModal = function (dialog) {
-        dialog.showModal();
+    const updateBoard = (move, playerToken) => {
+        const row = move[0];
+        const col = move[1];
+        board[row][col].setValue(playerToken);
+    } 
+
+    const validateMove = (move) => {
+        const row = move[0];
+        const col = move[1];
+        return board[row][col].getValue() === 0;
     }
 
-    //Handle when user clicks outside of a dialog to close it
-    const handleOutsideClick = function(event, dialog) {
-        const dialogDimensions = dialog.getBoundingClientRect();
-        if (
-            event.clientX < dialogDimensions.left ||
-            event.clientX > dialogDimensions.right ||
-            event.clientY < dialogDimensions.top ||
-            event.clientY > dialogDimensions.bottom
-        ) {
-            dialog.close()
-        } 
+    return {
+        printBoard,
+        updateBoard,
+        validateMove,
     }
+}
 
-    const handleNameFormSubmit = function(event, dialog, player1, player2) {
-        event.preventDefault();
+function UIController() {
+    const svgX = `
+    <svg fill="#a0b2a2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,7L11,12L9,17H11L12,14.5L13,17H15L13,12L15,7H13L12,9.5L11,7H9Z" /></svg>
+    `
+    const svgO = `
+    <svg fill="#f5f5f5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11,7A2,2 0 0,0 9,9V15A2,2 0 0,0 11,17H13A2,2 0 0,0 15,15V9A2,2 0 0,0 13,7H11M11,9H13V15H11V9Z" /></svg>
+    `
+    //Cache DOM
+    const p1TokenSVG  = document.getElementById("p1-token-svg")
+    const p2TokenSVG  = document.getElementById("p2-token-svg")
 
-        const p1NameInput = document.getElementById("p1-name-input");
-        const p2NameInput = document.getElementById("p2-name-input");
-
-        const p1Name = p1NameInput.value
-        const p2Name = p2NameInput.value
-
-        if(p1Name && p2Name) {
-            //If both values present when submitted, set both player names
-            player1.setName(p1Name);
-            player2.setName(p2Name);
-
-            //Log for testing purposes
-            console.log(`Player 1: ${player1.getName()}, Player 2: ${player2.getName()}`)
-
-            //Hide set-name-block in aside
-            setNameBlock.style.display = "none";
-
-            //Show player info in aside
-            p1Info.style.display = "block";
-            p2Info.style.display = "block";
-            
-            //Set display names
-            p1DisplayName.textContent = player1.getName();
-            p2DisplayName.textContent = player2.getName();
-
-            //Close name dialog
-            dialog.close()
+    renderPlayerTokens = function(p1token) {
+        if(p1token === "X") {   
+            p1TokenSVG.innerHTML = svgX;
+            p2TokenSVG.innerHTML = svgO;
+        }
+        else {
+            p1TokenSVG.innerHTML = svgO;
+            p2TokenSVG.innerHTML = svgX;
         }
 
     }
 
-    const setUpNameModal = function(player1, player2) {
-        const nameDialog = document.getElementById("name-dialog");
-        const setNameBtn = document.getElementById("set-name-btn");
-        const nameForm = document.getElementById("name-form");
-
-        // Add event listener to button that opens modal
-        setNameBtn.addEventListener("click", () => openModal(nameDialog));
-
-        //Add event listener for closing module when clicking outside it
-        nameDialog.addEventListener("click", (event => handleOutsideClick(event, nameDialog)))
-
-        // Add event listener to handle form submission
-        nameForm.addEventListener("submit" , (event) => handleNameFormSubmit(event, nameDialog, player1, player2))
-
-        //Open the name dialog by default
-        nameDialog.showModal();
-    }
-
-    const setUpTokenModal = function(player1, player2) {
-        const tokenDialog = document.getElementById("token-dialog");
-
-        //Open the token dialog by default
-    }
-
     return {
-        createBoardGrid,
-        setUpNameModal,
-        setUpTokenModal
+        renderPlayerTokens
     }
-} 
 
-function gameController() {
-    const toggleActivePlayer = () => {activePlayerIndex = (activePlayerIndex + 1) % 2} 
-
-    //Initialise gameboard and UI
-    const board = Gameboard();
-    const ui = UIConroller();
-
-    //Create UI grid
-    const rows = board.getRowCount();
-    const cols = board.getColCount();
-    ui.createBoardGrid(rows, cols);
-
-    // Initialise players
-    const player1 = Player();
-    const player2 = Player();
-    const players = [player1, player2];
 
 }
-    // player1.assignToken('X');
-    // player2.assignToken(player1.getToken() === "X" ? "O" : "X");
 
-    // console.log(`Player 1 has been assigned ${player1.getToken()}. Player 2 has been assigned ${player2.getToken()}.`)
+function gameController() {
+    //Initialise board and UI
+    const board = Gameboard();
+    const ui = UIController();
+    
+    //Initialise players
+    const player1 = Player('X')
+    const player2 = Player('O')
+    const players = [player1, player2];
+
+    //Render default tokens
+    ui.renderPlayerTokens(players[0].getToken())
 
     
-    // let activePlayerIndex = 0
-    // let moveCounter = 0
-    
-gameController();
+
+}
+
+let game = gameController();
+
+// const p1TokenSVG = document.getElementById("p1-token-svg");
+// const svgX = `
+// <svg fill="#a0b2a2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,7L11,12L9,17H11L12,14.5L13,17H15L13,12L15,7H13L12,9.5L11,7H9Z" /></svg>
+// `
+// p1TokenSVG.innerHTML = svgX;
 
 
+// let grid = document.getElementById("grid")
+// let OToken = document.getElementById("o-token")
+// let gridRows = grid.rows
+// let row1 = gridRows[0].cells;
+// row1[0].style = "background: url(/images/alpha-o.svg)"
+// .rows method for a table and .cells method for a row. access with index as usual. 
+// Use background url to get x or o svg
+
+//When game is started unbind event on player score buttons
+//assign tokens based on values of player buttons
